71f531d4cbb1a8c1328127e3ccb58b181f4370ce
Hal Fulcher <hallam.fulcher@gmail.com>
2021-01-22 18:54:44 +0000

    date.js module created and passed back into app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express  = require('express');
 const bodyParser = require('body-parser');
+const date = require(__dirname + '/date.js');
 
 const app = express();
 
 let items = ['Buy Food','Cook Food', 'Eat Food'];
+let workItems  = [];
 
 app.set('view engine', 'ejs');
 
@@ -13,18 +15,9 @@ app.use(express.static('public'));
 
 app.get('/', function(req, res){
 
-let today = new Date();
+    let day = date();
 
-
-let options = {
-    weekday: 'long',
-    day: 'numeric',
-    month: 'long'
-};
-
-let day = today.toLocaleDateString('en-US', options);
-
-res.render('list', {kindofDay: day, newListItems: items});
+res.render('list', {listTitle: day, newListItems: items});
 
 });
 
@@ -37,6 +30,15 @@ app.post('/', function(req, res) {
     res.redirect('/');
 });
 
+app.get('/work', function(req, res){
+res.render('list', {listTitle: 'Work List', newListItems: workItems});
+});
+
+app.post('/work', function(req, res) {
+let item = req.body.newItem;
+workItems.push(item);
+res.redirect('/work');
+});
 
 app.listen(3000, function(){
 console.log('Server started on port 3000');
